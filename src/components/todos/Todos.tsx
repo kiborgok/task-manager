d6bea58d35c10665b242ea9b7961382bbf34ad34
Alex Kiborgok <borgokalek14@gmail.com>
2023-11-01 22:28:29 +0300

    update component

--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -1,16 +1,12 @@
 import AddTodoForm from "./add-todo-form/AddTodoForm";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Todos = () => {
-  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
       <div className="text-lg text-orange-100" aria-roledescription="todos">
         <div className="max-w-[500px] mx-auto">
           <AddTodoForm />
         </div>
       </div>
-    </QueryClientProvider>
   );
 };
 
