bf139716af803bcfdfab87c4f57717633f27c103
Alex Kiborgok <borgokalek14@gmail.com>
2023-11-01 22:36:07 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import Navbar from "./components/navbar/Navbar";
+import Todos from "./components/todos/Todos";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <div className="bg-gray-800 min-h-screen py-4">
-        <div className="max-w-3xl mx-auto">
-          <Navbar />
+      <QueryClientProvider client={queryClient}>
+        <div className="bg-gray-800 min-h-screen py-4">
+          <div className="max-w-3xl mx-auto">
+            <Navbar />
+            <Todos />
+          </div>
         </div>
-      </div>
+      </QueryClientProvider>
     </>
   );
 }
