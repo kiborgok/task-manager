c3d19805ec5b0417668f8a5e8ab7e1be97c36fc5
Alex Kiborgok <borgokalek14@gmail.com>
2023-11-02 11:17:50 +0300

    add routing and auth  to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import Navbar from "./components/navbar/Navbar";
-import Todos from "./components/todos/Todos";
+import {useEffect} from "react";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import LoginForm from "./components/auth/LogIn";
+import SignUpForm from "./components/auth/SignUp";
+import Home from "./components/Home";
+import { useAuthUser } from "./stores/useAuthUser";
+import { UserType } from "./types/types";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const queryClient = new QueryClient();
+
+  const { setUser, user } = useAuthUser();
+  const storedUser = localStorage.getItem("auth_user");
+
+  useEffect(() => {
+    if (storedUser) {
+      const authUser: UserType = JSON.parse(storedUser);
+      setUser({ user: authUser });
+    }
+  }, [storedUser, setUser]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: user ? <Home /> : <Navigate to={"/login"} />,
+    },
+    {
+      path: "/login",
+      element: user ? <Navigate to={"/"} /> : <LoginForm />,
+    },
+    {
+      path: "/signup",
+      element: user ? <Navigate to={"/"} /> : <SignUpForm />,
+    },
+  ]);
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <div className="bg-gray-800 min-h-screen py-4">
-          <div className="max-w-3xl mx-auto">
-            <Navbar />
-            <Todos />
-          </div>
-        </div>
+        <RouterProvider router={router} />
+        <Toaster position="bottom-right" reverseOrder />
       </QueryClientProvider>
     </>
   );
